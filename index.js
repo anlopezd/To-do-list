12a3267fe11ea02433c0f4a5a6d0a198e1159d9c
anlopezd <91441753+anlopezd@users.noreply.github.com>
2021-12-05 19:12:21 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const input = document.querySelector("input");
 const addBtn = document.getElementById("btn");
 const ul = document.querySelector("ul");
 const empty = document.querySelector(".empty")
+const numbers = document.querySelector(".numbers")
 
 addBtn.addEventListener("click", (e) => {
     e.preventDefault()
+    
     const text = input.value;
     const textEdit = text.replace(text[0], text[0].toUpperCase())
+    const numeros = /^[0-9]/
+    
 
-    if (text !== "") {
+    if (text !== "" && !text.match(numeros)) {
         const li = document.createElement("li")
         const p = document.createElement("p")
         p.textContent = textEdit;
@@ -17,6 +21,9 @@ addBtn.addEventListener("click", (e) => {
         li.appendChild(p)
         input.value = ""
         empty.style.display = "none"
+        numbers.style.display = "none";
+    } else if (text.match(numeros)) {
+        numbers.style.display = "block";
     }
     
 
@@ -38,8 +45,5 @@ function deleteBtn() {
             empty.style.display = "block"
         }
     })
-
-    
-    
     return deleteBtn;
 }
